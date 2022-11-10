149021546c647af3908cde968bd329fccba8f7a1
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-11-10 15:40:37 -0700

    Create Edamam Service

---
+++ app/services/edamam_service.rb
@@ -0,0 +1,23 @@
+require 'faraday'
+require 'json'
+
+class EdamamService
+  def self.get_recipes(country)
+    response = self.conn.get('/api/recipes/v2', { q: country })
+    parse(response)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new('https://api.edamam.com') do |faraday|
+      faraday.params['type'] = 'public'
+      faraday.params['app_id'] = ENV['app_id']
+      faraday.params['app_key'] = ENV['app_key']
+    end
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
--- spec/services/edamam_service_spec.rb
+++ spec/services/edamam_service_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe EdamamService do
     expect(search[:hits]).to be_an Array
 
     search[:hits].each do |recipe|
-      expect(recipe).to have_key :uri
-      expect(recipe[:uri]).to be_a String
-      expect(recipe).to have_key :label
-      expect(recipe[:label]).to be_a String
-      expect(recipe).to have_key :image
-      expect(recipe[:image]).to be_a String
+      expect(recipe[:recipe]).to have_key :uri
+      expect(recipe[:recipe][:uri]).to be_a String
+      expect(recipe[:recipe]).to have_key :label
+      expect(recipe[:recipe][:label]).to be_a String
+      expect(recipe[:recipe]).to have_key :image
+      expect(recipe[:recipe][:image]).to be_a String
     end
   end
 end
