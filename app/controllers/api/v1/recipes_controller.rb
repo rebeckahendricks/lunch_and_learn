f64e7f3c5f47a435f41256243faebfc432fbf989
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-11-11 10:34:50 -0700

    Refactor RecipesController

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    country = params[:country]
-    country ||= CountryFacade.random_country
+    country = params[:country] ||= CountryFacade.random_country
     recipes = RecipeFacade.get_recipes(country)
     render json: RecipeSerializer.serialize(recipes, country)
   end
