b78037c85a4565d5fc067538cae316a1ebced272
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-11-11 09:49:18 -0700

    Reformat handrolled hash

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -6,17 +6,17 @@ class RecipeSerializer
   def self.serialize(recipes, country)
     {
       "data": recipes.map do |recipe|
-      {
-        "id": "null",
-        "type": "recipe",
-        "attributes": {
-          "title": recipe.title,
-          "url": recipe.url,
-          "country": country,
-          "image": recipe.image
+        {
+          "id": 'null',
+          "type": 'recipe',
+          "attributes": {
+            "title": recipe.title,
+            "url": recipe.url,
+            "country": country,
+            "image": recipe.image
+          }
         }
-      }
-    end
+      end
     }
   end
 end
