4e251af7de8372b34744c8a9dfa1cef8d6615176
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-11-12 13:02:53 -0700

    Refactor API key generation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,10 @@
+require 'securerandom'
+
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      api_key = Faker::Alphanumeric.alphanumeric(number: 27)
+      api_key = SecureRandom.urlsafe_base64
       user.update(api_key: api_key)
       render json: UserSerializer.new(user), status: 201
     else
