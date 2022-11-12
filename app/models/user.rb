ddd6e48b263b1aa55369b9f147714304920b2834
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-11-12 14:06:51 -0700

    Create Favorite model, spec, & migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 require 'securerandom'
 
 class User < ApplicationRecord
+  has_many :favorites
+  
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :api_key, uniqueness: true
--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -4,14 +4,29 @@ RSpec.describe 'Favorites Request API' do
   describe 'Happy Path' do
     it 'adds recipes to a favorited list for a user' do
       user = create(:user)
-      country = "italy"
       user.generate_api_key
       recipe = build(:recipe)
 
       request_body = {
-        "api_key": user.api_key,
-        "country": country
+        api_key: user.api_key,
+        country: recipe.country,
+        recipe_link: recipe.url,
+        recipe_title: recipe.title
       }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/favorites', headers: headers, params: JSON.generate(request_body)
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(201)
+
+      favorites_response = JSON.parse(response.body, symbolize_names: true)
+      expected_favorites_response = {
+        "success": 'Favorite added successfully'
+      }
+
+      expect(favorites_response).to eq(expected_favorites_response)
     end
   end
 end
