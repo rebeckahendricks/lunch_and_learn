91da4e16820b4d7cadfdfcf4a735278ad44f993c
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-11-12 15:05:30 -0700

    Fix Favorite factory

--- spec/factories/favorites.rb
+++ spec/factories/favorites.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :favorite do
-    country { "MyString" }
-    recipe_link { "MyString" }
-    recipe_title { "MyString" }
+    country { Faker::Address.country }
+    recipe_link { Faker::Internet.url }
+    recipe_title { Faker::Food.dish }
     user { nil }
   end
 end
